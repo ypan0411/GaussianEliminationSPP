e6442e282ec30ced1b804e0feed65240e21df8ff
Yu Pan <ypan@localhost.localdomain>
2017-02-19 18:53:59 +0100

    Cpp version1

---
+++ GaussianEliminationSPP/main.cpp
@@ -0,0 +1,88 @@
+/**
+ * This algorithm is used for computing gaussian elimination with
+ * scaled partial pivoting. For sake of simplicity, please consider
+ * vector b as part of the matrix.
+ * For example, if you are dealing with a 4*4 matrix and vector b
+ * which is 4*1, you simply add b to the right side of the matrix
+ * and making it 4*5.
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void printMatrix(vector<vector<double>> & matrix){
+    for(int i = 0; i < matrix.size(); i++){
+        for(int j = 0; j < matrix[0].size(); j++){
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void GaussianEliminationSSP(vector<vector<double>>& matrix, int n){
+    //Initialize vector s
+    vector<double> s;
+    cout << "Initialization of s: [ ";
+    for(int i = 0; i < n; i++){
+        //Consider all entries except the last one, which is storing value of vector b
+        double max = *max_element(matrix[i].begin(), matrix[i].end() - 1);
+        cout << max << " ";
+        s.push_back(max);
+    }
+    cout << "]" << endl;
+
+    for(int i = 0; i < n - 1; i++){
+        //Finding the row which the relative pivot element is the largest.
+        double max = matrix[i][i] / s[i];
+        int max_index = i;
+        for(int j = i + 1; j < n; j++){
+            if(max < matrix[j][i] / s[j]){
+                max = matrix[j][i] / s[j];
+                max_index = j;
+            }
+        }
+        //Switching two rows, as well as two elements in vector s
+        cout << "Switch row " << i+1 << " and " << max_index+1 << endl;
+        swap(matrix[i], matrix[max_index]);
+        swap<double>(s[i], s[max_index]);
+
+        //Do the forward elimination
+        for(int j = i + 1; j < n; j++){
+            double factor = matrix[j][i] / matrix[i][i];
+            matrix[j][i] = 0;
+            for(int k = i + 1; k <= n; k++){
+                matrix[j][k] = matrix[j][k] - factor * matrix[i][k];
+            }
+        }
+        cout << "Forward Elimination:" << endl;
+        printMatrix(matrix);
+
+    }
+
+
+}
+
+int main()
+{
+    int n;
+    vector<vector<double>> matrix;
+//    cout << "Scale of the matrix:";
+//    cin >> n;
+//    cout << "Enter the entries one by one" << endl;
+//    for(int i = 0; i < n; i++){
+//        matrix.push_back({});
+//        for(int j = 0; j <= n; j++){
+//            double entry;
+//            cin >> entry;
+//            matrix[i].push_back(entry);
+//        }
+//    }
+
+    matrix = {{1, 0, 4, 1, 1}, {2, 1, 10, 1, 2}, {-1, 5, 4, 3, 3}, {5, 2, 2, 2, 4}};
+    GaussianEliminationSSP(matrix, 4);
+
+    return 0;
+}
